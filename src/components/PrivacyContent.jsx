c2cd81b9b9012b39c6cb2fefdd89fd02fe66c580
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-04-26 17:12:29 +0530

    updates files

--- src/components/PrivacyContent.jsx
+++ src/components/PrivacyContent.jsx
@@ -45,9 +45,21 @@ const PrivacyContent = () => {
                         <li><a href="https://firebase.google.com/policies/analytics" target="_blank" rel="noreferrer" style={{textDecoration:"None", color:"white"}}>Google Analytics for Firebase</a></li>
 
                     </ul>
-                    <br/>Our service also provides in-app subscriptions which are of weekly, monthly and annual tenure called AVATARS AI-PRO. The AI Writing Assistant feature of the app comes at a limited usage of 10 prompts on our free plan. Users need to subscribe to the AVATARS AI-PRO subscription in order to increase their access count to an unprecedented high number for the AI Writing Assistant feature.
                 </Typography>
 
+
+
+
+                <Typography variant="body2" component="div" sx={{marginTop:'1rem'}}>
+                    <br/><br/>Fair Usage Policy
+                </Typography>
+                <Typography variant="body2" component="div" sx={{ opacity:"0.6"}}>
+                    With our free plan, each user who sign ups for the service, receives 10 FREE prompts which can be used free of cost.
+                    <br/>Our service also provides in-app subscriptions which are of weekly, monthly and limetime tenure called AVATARS AI-PRO. All PRO features are unlocked when a user purchases the AVATARS AI-PRO membership and the features stay unlocked as long as the subscription is active.
+                    
+                    For providing a better experience, we have few enforcements in place that's part of our Fair Usage Policy. Although you can generate unlimited words on the PRO plan, we don't allow multiple users sharing the same account and generation limits (20 req/day for any user) are there to deter automated/robotic behavior and prevent our API abuse. Our AI engine also restricts certain kinds of use cases/usage behavior and automatically adjusts some parameters temporarily depending on the abnormal, heavy usage pattern in a short time span. You might notice a deterioration in the quality of the output when such activity is detected by the system.
+                    Hence, to prevent API abuse we restrict users with 20req/day while using our service. If we notice any abnormal behavior or an attempt to voilate our Fair Usage Policy, we may temporarily put your account in an inactive stage during which you may not be able to use our service or may permanently ban you from using our service. By subscribing to our PRO plan, you agree to follow the Fair Usage Policy as mentioned while subscribing to the PRO plan. 
+                </Typography>
 
 
 
